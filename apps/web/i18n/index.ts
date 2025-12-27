e3af97f8b9d8c4927e89e10e5c67a8b9b4b27a38
aestetor <adilkhanomar2@gmail.com>
2025-12-28 03:38:14 +0500

    ehh

--- apps/web/i18n/index.ts
+++ apps/web/i18n/index.ts
@@ -2,8 +2,9 @@ import ru from "./messages/ru.json";
 
 export type Locale = "ru" | "en";
 
-const messages = {
-  ru
+const messages: Record<Locale, typeof ru> = {
+  ru,
+  en: ru
 };
 
 export const defaultLocale: Locale = "ru";
